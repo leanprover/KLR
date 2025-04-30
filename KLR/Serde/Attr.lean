c3bac42662905aed5d403a02eaa6f9d258338772
Paul Govereau <govereau@amazon.com>
2025-04-30 06:56:27 -0400

    feat: add serde attribute for defining serialized formats
    This changes add a new attribute for constructors: "serde tag=n".  The
    motivation is to provide an easy way to define how constructors should be
    mapped to numeric values in serialized formats.

---
+++ KLR/Serde/Attr.lean
@@ -0,0 +1,97 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Govereau, Sean McLaughlin
+-/
+import Lean
+
+/-
+# Attribute for serialization and de-serialization
+
+This modules defines a new attribute and some meta-programming utilities for
+accessing the attribute data. Note: new attributes must be defined in a
+separate module from where they are used. See `Test.lean` for more details.
+-/
+namespace KLR.Serde
+open Lean Meta
+
+private structure SerdeTag where
+  tag : Nat
+  deriving Inhabited, BEq, Repr
+
+syntax (name := serde) "serde" "tag" "=" num : attr
+
+private initialize tags : ParametricAttribute SerdeTag <-
+  registerParametricAttribute {
+    name := `serde
+    descr := "Assign Serde tag"
+    getParam name stx := do
+      let `(attr| serde tag = $t:num  ) := stx
+        | throwError "invalid [serde] attribute"
+      return ⟨ TSyntax.getNat t ⟩
+  }
+
+-- Return the serde tag for a name (if any)
+def serdeTag [Monad m] [MonadEnv m] (name : Name) : m (Option Nat) := do
+  return (tags.getParam? (<- getEnv) name).map (·.tag)
+
+-- Check for duplicates and reverse list (private function used below)
+private def checkDups (l : List (a × Nat)) : MetaM (List (a × Nat)) := do
+  let rec loop l1 l2 :=
+    match l1, l2 with
+    | l1, [] => return l1
+    | l1, x :: xs =>
+      if l1.any fun p => p.2 == x.2
+      then throwError s!"duplicate Serde tag found {x.2}"
+      else loop (x :: l1) xs
+  loop [] l
+
+/-
+Return a mapping of constructor names to serde tags for a given type. Any
+constructors without assigned tags will be automatically assigned a tag
+starting equal to the previous tag plus one, while avoiding any user defined
+values.
+
+One important use case is adding a new constructor. If we add a new constructor
+to a type, but we don't want to add it at the end, then we can manually assign
+it a tag and the other constructors will end up with their previous assignments.
+
+inductive A | a | c | d
+-- mapping is a=0, c=1, d=2
+
+-- changed to:
+inductive A | a | b | c | d
+-- mapping is: a=0, b=1, c=2, d=3  (INCORRECT)
+
+@[serde tag = 3 ] A.b
+-- mapping is: a=0, b=3, c=1, d=2  (correct)
+
+See Test.lean for more details.
+-/
+partial def nextTag (user : List (Option Nat)) (n : Nat) : Nat :=
+  let n := n + 1
+  if user.contains (some n)
+  then nextTag user n
+  else n
+
+def serdeMap (name : Name) : MetaM (List (Name × Nat)) := do
+  let tcs <- getConstInfoInduct name
+  let user <- tcs.ctors.mapM serdeTag
+  let nextTag := nextTag user
+  let mut current := 0
+  let mut res := []
+  for ctor in tcs.ctors do
+    match <- serdeTag ctor with
+    | none =>
+        res := (ctor, current) :: res
+        current := nextTag current
+    | some n =>
+        res := (ctor, n) :: res
+        current := nextTag n
+  checkDups res
+
+-- Convenience function: serdeTag and serdeMap
+def serdeTags (name : Name) : MetaM (Nat × List (Name × Nat)) := do
+  match <- serdeTag name with
+  | none => throwError s!"No serde tags for {name}"
+  | some t => return (t, <- serdeMap name)
