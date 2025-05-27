a8fe4078daa838eda35b3690ca8ec753d3506927
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-05-27 16:37:29 -0400

    define and prove correctness for IndexSpan.clip_comp

--- KLR/Core/Indexing.lean
+++ KLR/Core/Indexing.lean
@@ -13,6 +13,98 @@ def List.forall {α : Type _} (L : List α) (P : α → Prop) : Prop :=
   | .nil => True
   | .cons l L => P l ∧ List.forall L P
 
+/-- Get the length of the longest prefix satisfying P -/
+@[simp] def List.len_max_prefix {α : Type _} (L : List α) (P : α → Bool) : Nat :=
+  let rec F n L P : Nat :=
+    match L with
+    | .nil => n
+    | .cons l L => if P l then F (n + 1) L P else n
+  F 0 L P
+attribute [simp] List.len_max_prefix.F
+
+theorem List.len_max_prefix.len_mono {α : Type _} n (L : List α) (P : α → Bool) :
+    n ≤ List.len_max_prefix.F n L P := by
+  revert n
+  induction L
+  · simp
+  rename_i _ _ IH; intro n
+  simp; split
+  · apply Nat.le_trans (n.le_add_right 1) (IH _)
+  · exact Nat.le_refl n
+
+theorem List.len_max_prefix_init_add {α : Type _} n (L : List α) (P : α → Bool) :
+    List.len_max_prefix.F n L P = n + List.len_max_prefix.F 0 L P := by
+  revert n
+  induction L
+  · intro n
+    simp
+  rename_i _ _ IH; intro n
+  simp; split
+  · conv=> rhs; rw [IH]
+    rw [IH, Nat.add_assoc]
+  · rfl
+
+theorem List.len_max_prefix_eq_zero_iff {α : Type _} {L : List α} {P : α → Bool} :
+    L.len_max_prefix P = 0 ↔ (L = [] ∨ ∃ l L', L = .cons l L' ∧ ¬ P l) := by
+  apply Iff.intro
+  · intro H
+    cases L
+    · left; rfl
+    · right; rename_i l L; exists l; exists L
+      apply And.intro rfl
+      intro Hk
+      simp at H; have _ := H Hk
+      have _ := List.len_max_prefix.len_mono 1 L P
+      omega
+  · rintro (H | ⟨_, _, ⟨H, HP⟩⟩)
+    · simp [H]
+    · simp [H, HP]
+
+theorem List.len_max_prefix_gt_zero_head {α : Type _} {L : List α} {P : α → Bool} :
+    0 < L.len_max_prefix P → ∃ l L', L = .cons l L' ∧ P l := by
+  cases L; simp
+  rename_i l L
+  simp; split
+  · rename_i H; exact fun _ => H
+  · omega
+
+theorem List.len_max_prefix_le_length {α : Type _} {L : List α} {P : α → Bool} :
+    L.len_max_prefix P ≤ L.length := by
+  simp
+  induction L
+  · simp
+  rename_i l L IH
+  simp
+  split
+  · rw [List.len_max_prefix_init_add]; omega
+  · simp
+
+theorem List.len_max_prefix_prefix_holds  {α : Type _} {L : List α} {P : α → Bool} i :
+    (h : i < L.length) → i < L.len_max_prefix P → P (L[i]) := by
+  revert L
+  induction i
+  · intro L HL _
+    cases L
+    · simp at HL
+    rename_i _ _ IH; simp
+    rcases List.len_max_prefix_gt_zero_head IH with ⟨_, _, ⟨H, H'⟩⟩
+    simp at H
+    exact H.1 ▸ H'
+  rename_i i IH
+  simp
+  intro L
+  rcases L with _|⟨l, L⟩
+  · simp
+  simp
+  intro Hlen Hi
+  apply IH
+  simp
+  split at Hi
+  · rw [List.len_max_prefix_init_add, Nat.add_comm] at Hi
+    exact Nat.lt_of_add_lt_add_left Hi
+  · simp at Hi
+
+
 @[simp] def List.tails {α : Type _} (L : List α) : List (List α) :=
   match L with | .nil => .nil | .cons _ L => L :: L.tails
 
@@ -84,38 +176,178 @@ theorem AccessBasic.free_indices_length (b : AccessBasic) :
 
 /- ## Spans -/
 
-/-- The finite sequence of natural numbers `start + span * i` for `i < num`. -/
+/-- The finite sequence of natural numbers `start + step * i` for `i < num`. -/
 structure IndexSpan where
   start : Nat
-  span : Int
+  step : Int
   num : Nat
-  span_nz : span ≠ 0
-  get_nonneg : ∀ i : Nat, i < num → 0 ≤ start + span * i
+  step_nz : step ≠ 0
+  get_nonneg : ∀ i : Int, 0 ≤ i → i < num → 0 ≤ start + step * i
 
-def IndexSpan.get! (s : IndexSpan) (i : Nat) : Nat :=
-  Int.toNat <| s.start + s.span * i
+namespace IndexSpan
 
-/-- First value outside of the span.
+/-- Get an index from the sequence without doing any bounds checks -/
+@[simp] def get! (s : IndexSpan) (i : Int) : Int :=
+  s.start + s.step * i
 
-TODO: This is actually not true when the stride is negative and ends at zero!
-not even really sure if there's a way to implement that in hardware. -/
-def IndexSpan.extreme (s : IndexSpan) : Nat :=
-  Int.toNat <| s.start + s.span * s.num
+/-- First natural number that is outside the range in the direction of s.stride.
+The purpose of this function is to establish a slice bound for a given IndexSpan.
+
+NOTE: If the last step is on zero, this just doesn't work. OTOH, if the last
+step is greater than zero then anything between the last step (exclusive) and
+the (last + 1) step (inclusive) is a suitable bound. Since the last step is nonnegative,
+min(0, (last + 1) step) works whenever the last step is nonzero and it is impossible
+if the last step is zero. -/
+def extreme (s : IndexSpan) : Nat :=
+  Int.toNat <| s.start + s.num * s.step
+
+/-- As long as the last inbounds step is nonzero, the minimum of the next step and 0
+(ie IndexSpan.extreme) will be outside of the range of the IndexSpan so can serve as a bound.
+The num=0 and num=1 cases will be handled separately so is excluded from regular spans. -/
+def is_regular (s : IndexSpan) : Bool := 1 < s.num && 0 < s.start + s.step * (s.num - 1)
+def regular_wf (s : IndexSpan) : Prop := s.is_regular
+
+/-- Convert a span to a slice. This encoding is correct when the slice is regular. -/
+def regular_toSlice (s : IndexSpan) : Slice :=
+  ⟨s.start, s.extreme, s.step, s.step_nz⟩
+
+-- A common case for regular slices
+theorem pos_step_regular {s : IndexSpan} (Hstep : 0 < s.step) (Hnum : 1 < s.num) :
+    s.is_regular := by
+  simp [IndexSpan.is_regular]
+  apply And.intro Hnum
+  apply Int.add_pos_of_nonneg_of_pos
+  · exact Int.ofNat_zero_le s.start
+  · apply Int.mul_pos Hstep
+    omega
+
+/-- In some cases where the normal formula doesn't work, we can still compile
+an IndexSpan to a slice. Namely, if the IndexSpan has zero or one elements, it can
+always be compiled by taking steps of size 1 in the positive direction. -/
+def is_subsingleton (s : IndexSpan) : Bool := s.num <= 1
+def subsingleton_wf (s : IndexSpan) : Prop := s.is_subsingleton
+def subsingleton_toSlice (s : IndexSpan) : Slice :=
+  ⟨s.start, s.start + s.num, 1, Int.zero_ne_one.symm⟩
+
+/-- Compile an IndexSpan to a slice. -/
+def toSlice (s : IndexSpan) : Err Slice :=
+  if s.is_subsingleton then .ok s.subsingleton_toSlice else
+  if s.is_regular then .ok s.regular_toSlice else
+  .error "Cannot compile IndexSpan to slice"
 
 /-- The span [0, d) -/
-def IndexSpan.full (d : Nat) : IndexSpan where
-  start := 0
-  span := 1
-  num := d
-  span_nz := Int.zero_ne_one.symm
-  get_nonneg := by simp
+def full (d : Nat) : IndexSpan := ⟨0, 1, d, Int.zero_ne_one.symm, by simp; exact fun _ H _ => H⟩
+
+
+/- ## Composition of two IndexSpans
+
+An IndexSpan `(s1.comp s2) [i] = s1(s2[i])`
+
+This composition uses clipping semantics, ie, it always returns an IndexSpan which
+does "as much of (s1.comp s2) as possible". If starting out of bounds, the resulting
+`num` field will be zero (not an error). -/
+
+namespace comp
+
+variable (s1 s2 : IndexSpan)
+
+def start : Int :=
+  s1.step * s2.start + s1.start
+
+def step : Int :=
+  s1.step * s2.step
+
+/-- Means that `i.toNat` is an inbounds access of the IndexSpan `s`. -/
+def inbounds? (s : IndexSpan) (i : Int) : Bool := 0 ≤ i && i < s.num
+
+
+/-- Calculate the number of values i (starting at 0) such that `start + step  * i` is both
+nonnegative and less than s2.bound. This will be the maximum (clipped) number of steps
+that the composed IndexSpan can take.
+
+TODO: There is a closed form for this, but for now we just explicitly search for the
+maximum over all possible indices for s2. -/
+
+@[simp] def num : Nat := List.len_max_prefix (List.range s2.num) (inbounds? s1 ∘ s2.get! ∘ Int.ofNat)
+
+/-- Lemma to deal with the Int.toNat coercion in IndexSpan.clip_comp -/
+theorem start_neg_implies_num_zero (H : start s1 s2 < 0) : comp.num s1 s2 = 0 := by
+  apply Nat.eq_zero_of_not_pos; intro H'
+  rcases (List.len_max_prefix_gt_zero_head H') with ⟨l, L, ⟨H1, H2⟩⟩
+  generalize H3 : s2.num = x; cases x
+  · simp [H3] at H1
+  rename_i n
+  rw [H3, Nat.add_comm, List.range_add] at H1
+  have Hr : List.range 1 = [0] := by rfl
+  simp [Hr] at H1; clear Hr
+  rcases H1 with ⟨H4, H5⟩
+  simp [← H4, inbounds?] at H2
+  simp [start] at H
+  have _ := s1.get_nonneg s2.start (Int.ofNat_zero_le _) (Int.ofNat_lt.mpr H2)
+  omega
+
+-- TODO: That naming is unfortunate
+theorem num_le_ub : num s1 s2 ≤ s2.num := by
+  apply Nat.le_trans List.len_max_prefix_le_length
+  rw [List.length_range]
+  apply Nat.le_refl
+
+theorem get!_eq_comp : ∀ i : Int, start s1 s2 + step s1 s2 * i = s1.get! (s2.get! i) := by
+  intro i
+  simp [start, step, get!]
+  conv=> lhs; rw [Int.add_assoc, Int.add_comm, Int.add_assoc]
+  congr
+  rw [Int.add_comm, Int.mul_add]
+  congr 1
+  apply Int.mul_assoc
+
+end comp
+
+/-- Defines a clipped composition operator for IndexSpans -/
+def IndexSpan.clip_comp (s1 s2 : IndexSpan) : IndexSpan where
+  start := Int.toNat <| comp.start s1 s2
+  step := comp.step s1 s2
+  num := comp.num s1 s2
+  step_nz := Int.mul_ne_zero_iff.mpr ⟨s1.step_nz, s2.step_nz⟩
+  get_nonneg i Hlb Hub := by
+    rcases Decidable.em (0 ≤ (comp.start s1 s2)) with (H|H)
+    · rw [Int.ofNat_toNat, Int.max_eq_left H]
+      rw [comp.get!_eq_comp]
+      apply s1.get_nonneg _ _
+      · suffices comp.inbounds? s1 (s2.get! i) by
+          simp [comp.inbounds?] at this
+          apply this.2
+        unfold comp.num at Hub
+        have Hnum := @List.len_max_prefix_prefix_holds _ (List.range s2.num)
+                       (comp.inbounds? s1 ∘ s2.get! ∘ Int.ofNat)
+                       i.toNat ?G1 ((Int.toNat_lt Hlb).mpr Hub)
+        case G1 =>
+          apply Nat.lt_of_lt_of_le ((Int.toNat_lt Hlb).mpr Hub)
+          exact List.len_max_prefix_le_length
+        simp at Hnum
+        apply (Int.max_eq_left Hlb ▸ Hnum)
+      · apply s2.get_nonneg _ Hlb
+        apply Int.lt_of_lt_of_le Hub
+        apply Int.ofNat_le.mpr
+        exact comp.num_le_ub s1 s2
+    · exfalso
+      simp at H
+      have Hnum := comp.start_neg_implies_num_zero (H := H)
+      omega
+
+end IndexSpan
+
+
+
+
+/- ## Layouts -/
 
 /-- Algebraic representation of an affine function taking Nat^dim to Z -/
 structure Layout (dim : Nat) where
   offset  : Nat
-  strides : List Int
+  steps : List Int
   nums    : List Nat
-  strides_dim : strides.length = dim
+  steps_dim : steps.length = dim
   nums_dim : nums.length = dim
 
 /-- An element of ℕ^dim -/
@@ -129,7 +361,7 @@ def Coord.inbounds (s : Shape) (c : Coord s.fdim) : Prop :=
 
 /-- Look up a memory coordinate in a layout, without doing any bounds checks -/
 def Layout.get! d (l : Layout d) (c : Coord d) : Int :=
-  l.offset + (List.zipWith (fun i n => i * Int.ofNat n) l.strides c.coords).sum
+  l.offset + (List.zipWith (fun i n => i * Int.ofNat n) l.steps c.coords).sum
 
 /-- A list of IndexSpans with a given dimension -/
 structure FreeSpans (d : Nat) where
@@ -141,23 +373,14 @@ structure FreeSpans (d : Nat) where
 /-- Precompose a list of FreeSpans with a layout to get another layout -/
 def span_layout_comp {d} (f : FreeSpans d) (l : Layout d) : Layout d where
   offset  := -- Offset of l + Dot product of starts to each span and each layout
-    l.offset + (List.zipWith (fun i n => i * Int.ofNat n) l.strides (f.spans.map IndexSpan.start)).sum.toNat
-  strides := List.zipWith (· * ·) (f.spans.map IndexSpan.span) l.strides
-  nums    := sorry -- Bound; some kind of minimum, not really sure yet
-  strides_dim := by
+    l.offset + (List.zipWith (fun i n => i * Int.ofNat n) l.steps (f.spans.map IndexSpan.start)).sum.toNat
+  steps := List.zipWith (· * ·) (f.spans.map IndexSpan.step) l.steps
+  nums  := sorry -- Bound; some kind of minimum, not really sure yet
+  steps_dim := by
     sorry
   nums_dim := by
     sorry
 
-
---   get_nonneg := sorry
-
-def span_span_comp (s1 s2 : IndexSpan) : IndexSpan where
-  start := Int.toNat <| s1.span * s2.start + s1.start
-  span := s1.span * s2.span
-  num := sorry -- Bound, not sure yet
-  span_nz := sorry -- Not sure
-  get_nonneg := sorry -- Not sure
 
 /-
 Span Semantics
@@ -167,18 +390,18 @@ abbrev LayoutMap d := Layout d → Layout d
 
 def coord_toIndexSpan (x : Nat) : IndexSpan where
   start := x
-  span := 1
+  step := 1
   num := 1
-  span_nz := Int.zero_ne_one.symm
-  get_nonneg := by simp
+  step_nz := Int.zero_ne_one.symm
+  get_nonneg := by simp; omega
 
 def Slice.toIndexSpan (s : Slice) (size : nat) : IndexSpan where
   start := s.l
-  span := s.step
+  step := s.step
   num :=
     -- Calculate the number of steps we can take with clipping
     sorry
-  span_nz := s.wf
+  step_nz := s.wf
   get_nonneg := sorry
 
 def Index.toIndexSpan (i : Index) (size : Nat) : IndexSpan :=
@@ -214,7 +437,7 @@ def pattern_interp_free (p : AccessPattern) : LayoutMap p.fdim := fun l =>
   sorry -- Refactor this
   -- Layout.mk
   --   (p.offset + coeff_sum * l.offset).toNat
-  --   (l.strides.map (coeff_sum * ·))
+  --   (l.steps.map (coeff_sum * ·))
   --   sorry -- List of bounds
   --   (by -- Nuiscance
   --     rw [List.length_map]
@@ -249,7 +472,7 @@ The construction may need to be lifted to Err & exclude the "decrement down to 0
 def CompileIndex.par (t : TensorName) (s : IndexSpan) : AccessBasic where
     tensor := t
     indexes :=
-      let parIndex := .slice ⟨s.start, s.extreme, s.span, s.span_nz⟩
+      let parIndex := .slice ⟨s.start, s.extreme, s.step, s.step_nz⟩
       let freeIndices := t.shape.freeDims.map (.slice ∘ Slice.full)
       parIndex :: freeIndices
     lenWF := by simp
@@ -259,14 +482,14 @@ Construct an AccessPattern that will extract these values from each row. -/
 def CompileIndex.free_pairs (t : TensorName) (parNum : Nat) (l : Layout d) : AccessPattern where
   tensor := t
   parNum := parNum
-  freePattern := List.zipWith APPair.mk l.strides l.nums
+  freePattern := List.zipWith APPair.mk l.steps l.nums
   offset := l.offset
 
 def Layout.RowMajorForm (s : Shape) : Layout s.fdim where
   offset := 0
-  strides := s.freeDims.tails.map <| Int.ofNat ∘ nat_list_prod
+  steps := s.freeDims.tails.map <| Int.ofNat ∘ nat_list_prod
   nums := s.freeDims
-  strides_dim := by
+  steps_dim := by
     sorry
   nums_dim := by
     sorry
