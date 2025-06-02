d9d8e76cb27bb499012636bf42c9dc86b858dda3
Paul Govereau <govereau@amazon.com>
2025-06-02 15:52:38 -0400

    fix: build failure on fresh checkout

--- KLR/lakefile.lean
+++ KLR/lakefile.lean
@@ -8,3 +8,6 @@ lean_lib Util
 
 require plausible from git
   "https://github.com/leanprover-community/plausible" @ "v4.19.0"
+
+require TensorLib from git
+  "https://github.com/leanprover/TensorLib.git" @ "v0.0.10"
