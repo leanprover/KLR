745d2547e5326203247c2cfff014141cf27991a5
Juneyoung Lee <lebjuney@amazon.com>
2025-06-03 04:18:53 -0500

    Use memset instead
    The additional argument is Nat, which is uint32_t in its C++ implementation (InstMemset)

--- KLR/Core/Pretty.lean
+++ KLR/Core/Pretty.lean
@@ -78,7 +78,7 @@ instance : ToFormat Operator where
   format
   | .load => "load"
   | .save => "save"
-  | .const => "const"
+  | .memset x => f!"memset {x}"
   | .tensorScalar .. => "tensor_scalar{..}"
   | .tensorScalarAddr .. => "tensor_scalar_addr{..}"
 
