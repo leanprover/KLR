452f1b9c3e4824750db5edbc4b0a3c183436ae12
Paul Mure <paulmure2000@gmail.com>
2025-06-04 12:03:02 -0700

    feat: define a basic type system for NKI
    This PR adds a basic type system for NKI using De Bruijn indices to represent type variables.
    An alternative definition with nominal variable binders are also presented.

---
+++ KLR/NKI/TypeCheck.lean
@@ -0,0 +1,40 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Mure
+-/
+import KLR.NKI.Basic
+import KLR.NKI.Types
+
+namespace KLR.NKI
+
+  /-- These enforce two shape variable to be equal -/
+  def ShapeConstr (nnat : Nat) := Fin nnat → Option Nat
+
+  def ShapeConstr.update (sc : ShapeConstr nnat) (idx : Fin nnat) (val : Option Nat) : ShapeConstr nnat :=
+    λ x => if x = idx then val else sc x
+
+  inductive ShapeIsType : List Nat → ShapeConstr nnat → List (SNat nnat) → Prop
+    | nil {sc} : ShapeIsType [] sc []
+    | cons_const {sc tl tl' hd n} :
+        hd = n
+        → ShapeIsType tl sc tl'
+        → ShapeIsType (hd :: tl) sc (.const n :: tl')
+    | cons_param {sc tl tl' hd idx} :
+        (sc idx = .none ∨ sc idx = .some hd)
+        → ShapeIsType tl sc tl'
+        → ShapeIsType (hd :: tl) (sc.update idx (.some hd)) (.param idx :: tl')
+
+  inductive Value.IsType {nnat ntyp : Nat} : Value → ShapeConstr nnat → STyp nnat ntyp → Prop
+    | none {sc} : Value.IsType .none sc .none
+    | bool {sc b} : Value.IsType (.bool b) sc .bool
+    | int {sc n} : Value.IsType (.int n) sc .int
+    | float {sc n} : Value.IsType (.float n) sc .float
+    | string {sc s} : Value.IsType (.string s) sc .string
+    | ellipsis {sc t} : Value.IsType .ellipsis sc t
+    | tensor {sc shape dtypeStr snat dtype} :
+        ShapeIsType shape sc snat
+        → dtypeStr = dtype.toString
+        → Value.IsType (.tensor shape dtypeStr) sc (.tensor snat dtype)
+
+end KLR.NKI
