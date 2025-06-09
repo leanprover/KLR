3915ab9e556c80edf15c9788a041e7ee102544ec
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-06-09 10:46:45 -0400

    add iris dependency

--- KLR/lakefile.lean
+++ KLR/lakefile.lean
@@ -26,7 +26,7 @@ require plausible from git
   "https://github.com/leanprover-community/plausible" @ "v4.21.0"
 
 require TensorLib from git
-  "https://github.com/leanprover/TensorLib.git" @ "v0.0.12"
+  "https://github.com/leanprover/TensorLib.git" @ "v0.0.10"
 
--- Comment the above and uncomment this for local development
--- require TensorLib from "../../TensorLib"
+require iris from git
+  "https://github.com/leanprover-community/iris-lean.git"
