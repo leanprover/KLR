f0a8a1a15758e388e06876ee9e96224a4433b379
Chungha Sung <chunghs@amazon.com>
2025-06-18 21:11:09 +0000

    remove BIRs

--- KLR/BIR.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Paul Govereau, Sean McLaughlin
--/
-import KLR.BIR.Instruction
-import KLR.BIR.Instructions
-import KLR.BIR.Function
-import KLR.BIR.Compile
