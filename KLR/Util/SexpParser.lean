ab89004784baf5e9638435d85da6fb3a59932e10
Sean McLaughlin <seanmcl@amazon.com>
2025-07-14 19:31:44 -0700

    chore: Use Lean's Parsec/FileMap/etc for parsing
    We are trying to move parsers from home-grown stuff to
    a standard set of combinators. This is an experiment with
    Lean builtins like Parsec, Position, FileMap etc. Seems
    better to me.

--- KLR/Util/SexpParser.lean
+++ KLR/Util/SexpParser.lean
@@ -7,186 +7,94 @@ import Util.Common
 import Util.Sexp
 
 open KLR(get!)
+open Lean(FileMap)
+open Std.Internal.Parsec(many many1 many1Chars peek? peek! satisfy)
+open Std.Internal.Parsec.String(Parser pchar skipChar)
 
 namespace KLR.Util.Sexp.Parse
 
-structure ParsingState where
-  iter : String.Iterator
-  line : Nat
-  col : Nat
-  file : String
-  deriving Repr, Inhabited, Nonempty
+-- For debugging
+private def remaining [ToString α] (p : Parser α) (s : String) : String := match p s.mkIterator with
+ | .success it v => s!"success: {v}: {it.remainingToString}"
+ | .error it msg => s!"error: {msg}: {it.remainingToString}"
 
-namespace ParsingState
+def comment : Parser Unit :=
+  skipChar ';' <* many (satisfy fun c => c != '\n')
 
-def init (s : String) (file : String := "<input>") : ParsingState := {
-  iter := String.mkIterator s
-  line := 1
-  col := 0
-  file
-}
+-- There is probably a better way to do this
+partial def space : Parser Unit := fun it =>
+  if it.atEnd then .success it ()
+  else if it.curr == ';' then
+    match comment it with
+    | .success it _ => space it
+    | .error it msg => .error it msg
+  else if it.curr.isWhitespace then space it.next
+  else .success it ()
 
-def rest (s : ParsingState) : String := s.iter.remainingToString
+def atomChar : Parser Char :=
+  satisfy fun c => !c.isWhitespace && c != '(' && c != ')' && c != ';'
 
-def done? (s : ParsingState) : Bool := s.iter.atEnd
+def string : Parser String := many1Chars atomChar
+def atom : Parser Sexp := Functor.map Sexp.atom string
 
-end ParsingState
-
-abbrev PState := StM ParsingState
-
-def raise (msg : String) : PState a := do
-  let st ← get
-  throw s!"{st.file}:{st.line}:{st.col}: {msg}"
-
-def done? : PState Bool := return (<- get).done?
-
-def peek : PState Char := do
-  if <- done? then raise "peeking at empty string" else
-  return (<- get).iter.curr
-
-def lookingAt' (p : Char -> Bool) : PState Bool := do
-  if <- done? then raise "lookingAt empty string"
-  return p (<- peek)
-
-def lookingAt (c : Char) : PState Bool := lookingAt' fun c' => c == c'
-
-def pop : PState Char := do
-  if <- done? then raise "popping empty string"
-  let s <- get
-  let c := s.iter.curr
-  let iter := s.iter.next
-  let line := if c == '\n' then s.line + 1 else s.line
-  let col := if c == '\n' then 0 else s.col + 1
-  set { s with iter, line, col }
-  return c
-
-def pop' : PState Unit := do
-  let _ <- pop
-
-def skipSemicolonComment : PState Unit := do
-  let c <- pop
-  if c != ';' then raise "not at a ;"
-  repeat do
-    if <- done? then break
-    let c <- pop
-    if c == '\n' then break
-
-def skip : PState Unit := do
-  repeat do
-    if <- done? then break
-    let c <- peek
-    if c.isWhitespace then pop'
-    else if c == ';' then skipSemicolonComment
-    else break
-
-def consumeChar (c : Char) : PState Unit := do
-  let c' <- pop
-  if c != c' then raise s!"expected {c} got {c'}"
-
-def isAtomChar (c : Char) : Bool := !c.isWhitespace && c != ';' && c != '(' && c != ')'
-
-def parseAtom : PState String := do
-  let mut token := ""
-  let mut started := false
-  repeat do
-    if <- done? then break
-    let c <- peek
-    if !(isAtomChar c) then break
-    if !started then
-      started := true
-    token := token.push c
-    pop'
-  if !started then raise "Couldn't find an atom"
-  return token
-
-mutual
-partial def parseSexp : PState Sexp := do
-  skip
-  if <- done? then raise "expected s-expression"
-  let c <- peek
-  match c with
-  | '(' => do
-    pop'
-    let sexps <- parseSexpList
-    consumeChar ')'
-    return .list sexps
-  | ')' => raise "unexpected ')'"
-  | _ =>
-    let a <- parseAtom
-    return Sexp.atom a
-
-partial def parseSexpList : PState (List Sexp) := do
-  let mut sexps := []
-  repeat do
-    skip
-    if <- (done? <||> lookingAt ')') then break
-    sexps := (<- parseSexp) :: sexps
-  return sexps.reverse
-end
-
-def parseMany : PState (List Sexp) := do
-  let mut sexps := []
-  while !(<- done?) do
-    let s <- parseSexp
-    sexps := s :: sexps
-    skip
-  return sexps.reverse
+-- Parse space up to a sexp
+partial def sexp : Parser Sexp :=
+  atom <|> list
+ where
+  list' := skipChar '(' *> space *> many (sexp <* space) <* skipChar ')'
+  list := Functor.map (fun arr => Sexp.list arr.toList) list'
 
 def defaultFilename : String := "<input>"
 
-def parseListFromString (s : String) (file : String := defaultFilename) : Except String (List Sexp) :=
-  let state := ParsingState.init s file
-  match parseMany.run state with
-  | .ok sexps state =>
-    match skip.run state with
-    | .ok _ state => if state.done? then return sexps else throw s!"Unexpected input after {state.line}, column {state.col}: "
-    | .error msg _ => throw msg
-  | .error msg _ => throw msg
+def fromString' (p : Parser α) (s : String) (filename : String := defaultFilename) : Err α := do
+  let filemap := FileMap.ofString s
+  match p s.mkIterator with
+  | .success it v =>
+    if it.remainingBytes == 0 then return v else throw s!"Remaining input: {it.remainingToString}"
+  | .error it msg =>
+    let pos := filemap.toPosition it.pos
+    throw s!"{filename}:{pos.line}:{pos.column}: {msg}"
 
-def parseSexpFromString (input : String) (file : String := defaultFilename) : Err Sexp := do
-  match <- parseListFromString input file with
-  | [s] => return s
-  | [] => throw "No sexps"
-  | _ :: _ => throw "Found multiple sexps"
+def fromString (s : String) (filename : String := defaultFilename) : Err Sexp :=
+  fromString' (space *> sexp <* space) s filename
 
-def parseSexpFromFile (file : String) : IO Sexp := do
-  parseSexpFromString (<- IO.FS.readFile file) file
+def fromString! (s : String) (filename : String := defaultFilename) : Sexp :=
+  get! $ fromString s filename
 
-def parseListFromFile (filepath : String) : IO (List Sexp) := do
-  parseListFromString (<- IO.FS.readFile filepath) filepath
+def listFromString (s : String) (filename : String := defaultFilename) : Except String (List Sexp) :=
+  let p := Functor.map Array.toList (many1 (space *> sexp <* space))
+  fromString' p s filename
 
-#guard parseSexpFromString "hello" == .ok (sexp%hello)
-#guard parseSexpFromString "(hello world)" == .ok (sexp%(hello world))
-#guard parseSexpFromString "(🏎️ 🏎️ 🏎️ 🏎️)" == .ok (sexp%("🏎️" "🏎️" "🏎️" "🏎️"))
-#guard parseSexpFromString "(hello world)   " == .ok (sexp%(hello world))
-#guard parseSexpFromString "(hello world) ; trailing comment" == .ok (sexp%(hello world))
-#guard parseSexpFromString "(a (b c) d)" == .ok (sexp% (a (b c) d))
-#guard parseSexpFromString "(hello ; this is a comment\n world)" == .ok (sexp%(hello world))
-#guard parseListFromString "atom1 (list item) atom2" == .ok [sexp% atom1, sexp% (list item), sexp% atom2]
-#guard parseListFromString "first ;\nsecond" == .ok [sexp% first, sexp% second]
-#guard parseListFromString "first ; comment\nsecond ; another comment\n(third fourth)" == .ok [sexp% first, sexp% second, sexp%(third fourth)]
-#guard parseSexpFromString "hello" == .ok (sexp% hello)
-#guard parseSexpFromString "(a b c)" == .ok (sexp% (a b c))
-#guard parseSexpFromString "(a (b c) d)" == .ok (sexp% (a (b c) d))
-#guard parseSexpFromString "(hello ; this is a comment\n world)" == .ok (sexp% (hello world))
-#guard parseListFromString "atom1 (list item) atom2" == .ok [sexp% atom1, sexp% (list item), sexp% atom2]
-#guard parseListFromString "first ; comment\nsecond ; another comment\n(third fourth)" == .ok [sexp% first, sexp% second, sexp% (third fourth)]
-#guard parseSexpFromString "()" == .ok (sexp% ())
-#guard match parseSexpFromString "(" with | .error _ => true | .ok _ => false
-#guard parseSexpFromString "  \t\n  (  a   b  \t c  )  \n  " == .ok (sexp% (a b c))
-#guard parseSexpFromString "(a b c)   " == .ok (sexp% (a b c))
-#guard parseSexpFromString "atom   " == .ok (sexp% atom)
-#guard parseListFromString "first ;\nsecond" == .ok [sexp% first, sexp% second]
-#guard parseListFromString "first ; comment\nsecond" == .ok [sexp% first, sexp% second]
-#guard get! (parseSexpFromString "(define (factorial n) ; recursive factorial function\n  (if (= n 0) ; base case\n      1\n      (* n (factorial (- n 1)))))")
-   == sexp%(define (factorial n) ("if" ("=" n 0) 1 ("*" n (factorial ("-" n 1)))))
-#guard match parseSexpFromString ")" with | .error _ => true | .ok _ => false
-#guard match parseSexpFromString "(a b" with | .error _ => true | .ok _ => false
-#guard match parseSexpFromString "" with | .error _ => true | .ok _ => false
-#guard parseSexpFromString "hello-world" == .ok (sexp% "hello-world")
-#guard parseSexpFromString "test123" == .ok (sexp% test123)
-#guard parseSexpFromString "a.b.c" == .ok (sexp% "a.b.c")
-#guard parseSexpFromString "(((((a)))))" == .ok (sexp% (((((a))))))
-#guard parseListFromString "(first list) atom (second list)" == .ok [sexp% (first list), sexp% atom, sexp% (second list)]
+def listFromString! (s : String) (filename : String := defaultFilename) : List Sexp :=
+  get! $ listFromString s filename
+
+def fromFile (file : String) : IO Sexp := do
+  fromString (<- IO.FS.readFile file) file
+
+def listFromFile (filepath : String) : IO (List Sexp) := do
+  listFromString (<- IO.FS.readFile filepath) filepath
+
+#guard fromString! "hello" == sexp%hello
+#guard fromString! "hello " == sexp%hello
+#guard fromString! " hello" == sexp%hello
+#guard fromString! "()" == sexp%()
+#guard fromString! "(hello world)" == sexp%(hello world)
+#guard fromString! "(🏎️ 🏎️ 🏎️ 🏎️)" == sexp%("🏎️" "🏎️" "🏎️" "🏎️")
+#guard fromString! "(hello world)   " == sexp%(hello world)
+#guard fromString! "(a (b c) d)" == sexp% (a (b c) d)
+#guard fromString! "(hello world) ; trailing comment" == sexp%(hello world)
+#guard fromString! "  \t\n  (  a   b  \t c  )  \n  " == sexp% (a b c)
+#guard fromString! "hello-world" == sexp% "hello-world"
+#guard fromString! "a.b.c" == sexp% "a.b.c"
+#guard fromString! "(((((a)))))" == sexp% (((((a)))))
+#guard listFromString! "(first list) atom (second list)" == [sexp% (first list), sexp% atom, sexp% (second list)]
+#guard listFromString! "atom1 (list item) atom2" == [sexp% atom1, sexp% (list item), sexp% atom2]
+#guard fromString! "(a b c)   " == sexp% (a b c)
+#guard listFromString! "first ;\nsecond" == [sexp% first, sexp% second]
+#guard listFromString! "first ; comment  \n second" == [sexp% first, sexp% second]
+#guard fromString! "(hello ; this is a comment\n world)" == sexp%(hello world)
+#guard listFromString! "first ;\nsecond" == [sexp% first, sexp% second]
+#guard listFromString! "first ; comment\nsecond ; another comment\n(third fourth)" == [sexp% first, sexp% second, sexp%(third fourth)]
+#guard fromString! "(define (factorial n) ; recursive factorial function\n  (if (= n 0) ; base case\n      1\n      (* n (factorial (- n 1)))))" == sexp%(define (factorial n) ("if" ("=" n 0) 1 ("*" n (factorial ("-" n 1)))))
 
 end KLR.Util.Sexp.Parse
