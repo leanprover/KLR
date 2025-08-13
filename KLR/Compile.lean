f5fa3f229a4c4aebd7cb6f61284124a1df2a292f
Michael Graeb <graebm@amazon.com>
2025-08-13 14:47:35 -0700

    trace FFI function can return errors also move some stuff back to Main.lean to minimize this diff

--- KLR/Compile.lean
+++ KLR/Compile.lean
@@ -23,89 +23,6 @@ open System(FilePath)
 def eprintln [ToString a] (debug : Bool) (x : a) : IO Unit := do
   if debug then IO.eprintln x
 
-structure FileNameParts where
-  private mk::
-  dirs : List String
-  fileNameNoExt : String
-  ext : Option String
-  deriving BEq, Inhabited, Repr
-
-namespace FileNameParts
-
-def fileName (parts : FileNameParts) : String := match parts.ext with
-| none => parts.fileNameNoExt
-| some ext => parts.fileNameNoExt ++ "." ++ ext
-
-def make (file : String) : FileNameParts :=
-  let (dirs, file) := match (file.splitOn "/").reverse with
-  | [] => impossible
-  | [f] => ([], f)
-  | f :: dirs => (dirs.reverse, f)
-  let (file, ext) := match (file.splitOn ".").reverse with
-  | [] => impossible
-  | [f] => (f, none)
-  | ext :: fs => (String.intercalate "." fs.reverse, some ext)
-  FileNameParts.mk dirs file ext
-
-def dir (parts : FileNameParts) : String := String.intercalate "/" parts.dirs
-
-def pathToFile (parts : FileNameParts) : String :=
-  String.intercalate "." (parts.dirs ++ [parts.fileNameNoExt] ++ parts.ext.toList)
-
-#guard FileNameParts.make "foo" == FileNameParts.mk [] "foo" none
-#guard FileNameParts.make "foo/bar" == FileNameParts.mk ["foo"] "bar" none
-#guard FileNameParts.make "a/b/c.bar" == FileNameParts.mk ["a", "b"] "c" (some "bar")
-#guard (FileNameParts.make "a.b.c.bar").pathToFile == "a.b.c.bar"
-#guard (FileNameParts.make "a/b/c.bar").dir == "a/b"
-
-end FileNameParts
-
-/-
-If we are in a python environment with a pip installed version of klr-lang,
-then we should have a program called klr-gather on the path; this script is
-installed in the python bin directory as part of the wheel installation. If we
-don't find the program on the PATH, then we try to use ./bin/gather, which will
-work for local developers using "lake exe klr".
--/
-def gatherRun (moduleFileName kernelFunctionName outputFileName: String)
-              (klrPythonModuleDir : Option String) (debug : Bool) : IO Unit := do
-  let dbg := eprintln debug
-  let pypath <- IO.getEnv "PYTHONPATH"
-  let pypath := pypath.getD ""
-  let parts := FileNameParts.make moduleFileName
-  dbg $ "parts: " ++ repr parts
-  let gather := FilePath.mk "klr-gather"
-  let localBin := (<- IO.currentDir).join "bin"
-  -- The directory of the kernel file must be on PYTHONPATH
-  let pypath := match parts.dirs with
-  | [] => pypath
-  | _ => pypath ++ ":" ++ parts.dir
-  -- The klr directory must also be there. A better implementation would check to see if it's on
-  -- the path already without adding `interop`
-  let klrDir := klrPythonModuleDir.getD "interop" -- interop is the project default
-  let pypath := pypath ++ ":" ++ klrDir
-  dbg $ "pypath: " ++ pypath
-
-  let gatherArg := #[ parts.fileNameNoExt, kernelFunctionName, outputFileName ]
-  let path <- IO.getEnv "PATH"
-  let paths := path.get!.splitOn ":"
-  let paths := paths.map FilePath.mk ++ [localBin]
-  for p in paths do
-    let exe := p.join gather
-    dbg $ "exe: " ++ exe.toString
-    if <- exe.pathExists then
-      let output <- IO.Process.output {
-        cmd := exe.toString
-        args := gatherArg
-        env := #[ ("PYTHONPATH", some pypath) ]
-      }
-      if output.exitCode != 0 then
-        IO.println output.stderr
-        IO.throwServerError "error gathering kernel"
-      dbg $ "stderr: " ++ output.stderr
-      return ()
-  IO.throwServerError "could not execute gather program"
-
 -- TODO: preserve warnings and errors
 def compilePython (kernel : Python.Kernel) : IO Core.Kernel := do
   let (kernel, warnings) := kernel.inferArguments
@@ -125,50 +42,45 @@ def compilePython (kernel : Python.Kernel) : IO Core.Kernel := do
   let kernel <- Core.lowerAccessPatterns kernel
   return kernel
 
-@[export frontend_compile_python]
-unsafe def frontend_compile_python
-    (moduleFileName kernelFunctionName klrPythonModuleDir: String)
-    (debug : UInt8) : UInt32 :=
-  let d := debug != 0
-  let module := moduleFileName
-  let name := kernelFunctionName
-  let moduleDir := klrPythonModuleDir
-  let x := unsafeEIO do
-      IO.FS.withTempFile fun _ tmpName => do
-        gatherRun module name tmpName.toString moduleDir d
-        let kernel <- KLR.File.readKLRFile tmpName .cbor
-        let _ <- compilePython kernel
-        return 0
-  match x with
-  | .ok n => n
-  | .error _ => 1
+-- reads srcPythonAstFileName, writes dstKlrFileName, returns kernel info as string of json
+@[export klr_frontend_trace]
+def frontend_trace (srcPythonAstFileName dstKlrFileName : String) : IO String := do
+  let kernel <- KLR.File.readKLRFile srcPythonAstFileName
+  let kernel <- compilePython kernel
+  let f := FilePath.mk (dstKlrFileName)
+  File.writeKLRFile f .cbor kernel
 
-@[export frontend_trace]
-unsafe def frontend_trace
-    (fname : String) : UInt32 :=
-  let r := unsafeEIO do
-    let kernel <- KLR.File.readKLRFile fname
-    let kernel <- compilePython kernel
-    IO.println (reprStr kernel)
-    return 0
-  let _ := unsafeEIO do
-    match r with
-    | .error e => IO.eprintln e
-    | _ => .ok ()
-  match r with
-  | .ok rv => rv
-  | .error _ => 1
+  -- return kernel info as JSON string
+  let inputsJson := kernel.inputs.map fun inp =>
+    Lean.Json.mkObj [
+      ("name", Lean.toJson inp.name),
+      ("dtype", Lean.toJson (reprStr inp.dtype)),
+      ("shape", Lean.toJson inp.shape)
+    ]
 
-@[export frontend_print]
-def frontend_print : IO UInt32 := do
-    IO.println ("hello")
-    return 0
+  let outputsJson := kernel.outputs.map fun out =>
+    Lean.Json.mkObj [
+      ("name", Lean.toJson out.name),
+      ("dtype", Lean.toJson (reprStr out.dtype)),
+      ("shape", Lean.toJson out.shape)
+    ]
 
--- @[export frontend_print]
--- def frontend_print : UInt32 := do
---     let r := unsafeEIO do
---       IO.println ("hello")
---       return ()
---     match r with
---     | .ok rv => rv
---     | .error _ => 1
+  let finalJson := Lean.Json.mkObj [
+    ("name", Lean.toJson kernel.name),
+    ("inputs", Lean.Json.arr inputsJson.toArray),
+    ("outputs", Lean.Json.arr outputsJson.toArray)
+  ]
+
+  return toString finalJson
+
+
+-- for testing basic FFI
+@[export klr_frontend_hello]
+def frontend_hello : IO UInt32 := do
+  IO.println ("hello from Lean")
+  return 0
+
+-- for testing FFI error handling
+@[export klr_frontend_fail]
+def frontend_fail : IO UInt32 := do
+  throw (IO.userError "frontend_fail 😵")
