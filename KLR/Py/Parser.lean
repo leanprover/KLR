c7999963f8d54be2d02202efef6e2cbbd106ede8
Paul Mure <paulmure2000@gmail.com>
2025-08-16 00:58:23 -0700

    c

--- KLR/Py/Parser.lean
+++ KLR/Py/Parser.lean
@@ -17,10 +17,47 @@ limitations under the License.
 import KLR.Py.Basic
 import KLR.Py.Tokenizer
 import KLR.Py.Pretty
+import Std.Data.HashMap.Basic
 
 namespace KLR.Py.Parser
 
 open Tokenizer (Token)
+
+def Token.hash (t : Token) : UInt64 :=
+  let s :=
+    match t.kind with
+    | .tokenLit s => s
+    | .ident _    => "ident"
+    | .int _      => "int"
+    | .float _    => "float"
+    | .string _   => "string"
+    | .newline    => "newline"
+    | .indent     => "indent"
+    | .dedent     => "dedent"
+  s.hash
+
+def Token.kindEq (t1 t2 : Token) : Bool :=
+  match t1.kind, t2.kind with
+  | .tokenLit x, .tokenLit y => x == y
+  | .ident _, .ident _
+  | .int _, .int _
+  | .float _, .float _
+  | .string _, .string _
+  | .newline, .newline
+  | .indent, .indent
+  | .dedent, .dedent => true
+  | _, _ => false
+
+def TokenMap (α : Type) := @Std.HashMap Token α ⟨Token.kindEq⟩ ⟨Token.hash⟩
+
+def TokenMap.emptyWithCapacity {α} (c : Nat) : TokenMap α :=
+  @Std.HashMap.emptyWithCapacity Token α ⟨Token.kindEq⟩ ⟨Token.hash⟩ c
+
+instance {α} : Singleton (Token × α) (TokenMap α) where
+  singleton a := (TokenMap.emptyWithCapacity 8).insert a.1 a.2
+
+instance {α} : Insert (Token × α) (TokenMap α) where
+  insert a m := m.insert a.1 a.2
 
 structure Context where
   input    : Array Token
@@ -121,10 +158,6 @@ instance {α β} : HAndThen (ParserM α) (ParserM β) (ParserM (α × β)) where
     let b ← b ()
     pure (a, b)
 
-def token (s : String) : ParserM Unit := do
-  let some ⟨.tokenLit curr, _⟩ ← peek | fail
-  if curr == s then consume else fail
-
 def not {α} (p : ParserM α) (msg : String) : ParserM Unit := fun c s =>
   match c.input[s.pos]? with
   | none => (some (), s)
@@ -163,9 +196,147 @@ def sepBy1 {α β} (p : ParserM α) (sep : ParserM β) (allowTrailing : Bool := 
 def sepBy {α β} (p : ParserM α) (sep : ParserM β) (allowTrailing : Bool := true) : ParserM (Array α) :=
   (fun as => as.getD #[]) <$> optional (sepBy1 p sep allowTrailing)
 
+def mkTokenMap {α} (l : List (String × α)) : TokenMap α :=
+  let l := l.map fun (t, a) => (⟨.tokenLit t, {}⟩, a)
+  l.foldl
+    (fun s (t, a) => s.insert t a)
+    (.emptyWithCapacity l.length)
+
+def token (s : String) : ParserM Unit := do
+  let some ⟨.tokenLit curr, _⟩ ← peek | fail
+  if curr == s then consume else fail
+
 def name : ParserM Ident := do
   let some ⟨.ident id, _⟩ ← peek | fail
   consume; return id
+
+def numLit : ParserM Exp' := do
+  let some ⟨t, _⟩ ← peek | fail
+  let some e :=
+    (match t with
+      | .int n   => Exp'.value <| .int n
+      | .float n => Exp'.value <| .float n
+      | _        => none) | fail
+  consume; return e
+
+def strLit : ParserM Exp' := do
+  let some ⟨.string s, _⟩ ← peek | fail
+  consume; return .value <| .string s
+
+inductive Assoc
+  | l | r | n
+deriving BEq
+
+instance : ToString Assoc where
+  toString | .l => "l" | .r => "r" | .n => "n"
+
+def Assoc.prec (n : Nat) : Assoc → Nat
+  | .l => n | .r => n - 1 | .n => n
+
+-- def Assoc.satPrec (prec : Nat) (precLimit : Nat) : Assoc → Bool
+--   | .l | .r => prec > precLimit
+-- | .n      => prec > precLimit + 1
+
+mutual
+
+partial def tuple : ParserM Exp := do
+  let some ⟨.tokenLit "(", pos⟩ ← peek | fail
+  consume
+  let some hd ← optional exp | do {
+    let some ⟨.tokenLit ")", stopPos⟩ ← peek | fail
+    consume
+    return ⟨⟨pos.pos, stopPos.stopPos⟩, .tuple []⟩
+  }
+  let some () ← optional (token ",") | { _ ← token ")"; return hd }
+  let tl ← sepBy exp (token ",")
+  let some ⟨.tokenLit ")", stopPos⟩ ← peek | fail
+  consume
+  return ⟨⟨pos.pos, stopPos.stopPos⟩, .tuple (hd :: tl.toList)⟩
+
+partial def list : ParserM Exp := do
+  let some ⟨.tokenLit "[", pos⟩ ← peek | fail
+  consume
+  let es ← sepBy exp (token ",")
+  let some ⟨.tokenLit "]", stopPos⟩ ← peek | fail
+  consume
+  return ⟨⟨pos.pos, stopPos.stopPos⟩, .list es.toList⟩
+
+partial def atom : ParserM Exp := do
+  -- Don't know if the compiler is smart enough to cache this
+  let atomMap : TokenMap (ParserM Exp) := {
+    (⟨.ident    ""     , {}⟩, withPos .mk <| Exp'.var <$> name),
+    (⟨.int      0      , {}⟩, withPos .mk <| numLit),
+    (⟨.float    0      , {}⟩, withPos .mk <| numLit),
+    (⟨.tokenLit "True" , {}⟩, withPos .mk <| (fun () => .value (.bool true)) <$> consume),
+    (⟨.tokenLit "False", {}⟩, withPos .mk <| (fun () => .value (.bool false)) <$> consume),
+    (⟨.string   ""     , {}⟩, withPos .mk <| strLit),
+    (⟨.tokenLit "("    , {}⟩,                tuple),
+    (⟨.tokenLit "["    , {}⟩,                list)
+  }
+
+  let some curr ← peek | fail
+  let some p := atomMap.get? curr | fail
+  p
+
+partial def unaryOpMap : TokenMap (Nat × UnaryOp) :=
+  mkTokenMap [
+    ("-", (100, .neg)),
+    ("+", (100, .pos)),
+    ("~", (100, .bitwiseNot)),
+    ("not", (55, .not)),
+  ]
+
+partial def binOpMap : TokenMap (Nat × Assoc × BinOp) :=
+  mkTokenMap [
+    ("**", (95, .r, .pow)),
+    ("*", (90, .l, .mul)),
+    ("+", (85, .l, .add)),
+    (">=", (60, .n, .ge)),
+  ]
+
+partial def exp (precLimit : Nat := 0) : ParserM Exp := do
+  let some start ← peek | fail
+  dbg_trace "exp called: tk={start.kind} precLimit={precLimit}"
+  let fst : Exp ←
+    match unaryOpMap.get? start with
+    | some (prec, op) =>
+      if prec + 1 ≤ precLimit then
+        pushErr "precedence level is low, consider parentheses" start.pos
+        fail
+      else
+        consume
+        let lhs ← exp prec
+        pure ⟨⟨start.pos.pos, lhs.pos.stopPos⟩, .unaryOp op lhs⟩
+    | none => atom
+
+  let mut lhs := fst
+  let mut curr ← peek
+  let mut precLimit := precLimit
+
+  while h : curr.isSome do
+    let tk := curr.get h
+    match binOpMap.get? tk with
+    | some (prec, assoc, op) =>
+      dbg_trace "op matched: op=({op.reprPrec.1}) assoc={assoc} prec={prec} precLimit={precLimit}"
+      if prec ≤ precLimit then
+        if assoc == .n then
+          pushErr "chaining of comparison operators is not allowed" tk.pos
+        else
+          pushErr "precedence level is low, consider parentheses" tk.pos
+        break
+      else
+        consume
+        let rhs ← exp (assoc.prec prec)
+        dbg_trace "rhs"
+        lhs := ⟨⟨start.pos.pos, rhs.pos.stopPos⟩, .binOp op lhs rhs⟩
+        if assoc == .n then precLimit := prec + 1
+    | none =>
+      break
+    curr ← peek
+
+  return lhs
+
+end
 
 def dottedName : ParserM QualifiedIdent := do
   not (token ".") "implcit current directory not supported"
@@ -178,6 +349,7 @@ def asName : ParserM (Option Ident) := do
   not (token "as") "missing qualifier after 'as'"
   return Prod.snd <$> as
 
+/-- Assumes the `import` keyword is already matched -/
 def imprt : ParserM Stmt' := do
   consume
   let mod ← dottedName
@@ -185,6 +357,7 @@ def imprt : ParserM Stmt' := do
   let as ← asName
   return .imprt mod as
 
+/-- Assumes the `from` keyword is already matched -/
 def fromImprt : ParserM Stmt' := do
   consume
   let mod ← dottedName
@@ -194,15 +367,16 @@ def fromImprt : ParserM Stmt' := do
   let as ← asName
   return .imprtFrom mod imp as
 
-def stmt : ParserM Stmt := withPos Stmt.mk do
+def stmt : ParserM Stmt := withPos .mk do
   let some curr ← peek | fail
 
   match curr.kind with
   | .tokenLit "import" =>
-    withErr "unfinised import statement" curr.pos imprt
+    withErr "unfinished import statement" curr.pos imprt
   | .tokenLit "from" =>
     withErr "unfinished import statement" curr.pos fromImprt
-  | _ => fail
+  | _ =>
+    withErr "failed to parse expression" curr.pos (.exp <$> exp)
   -- | .tokenLit "def" =>
   --   sorry
   -- | .tokenLit t =>
@@ -234,10 +408,54 @@ def run (input : String) (fileName : String) (fileMap : Lean.FileMap := input.to
   | none =>
     .error <| s.err.getLast?.getD "invalid syntax"
 
+namespace Debug
 
-namespace _Debug
+mutual
 
-def str := "import a.b.c as b"
--- #eval run str "<input>"
+def Exp.printTree (e : Exp) : Std.Format :=
+  match e with
+  | { exp, .. } => Exp'.printTree exp
 
-end _Debug
+def Exp'.printTree (e : Exp') : Std.Format :=
+  match e with
+  | .binOp op x y =>
+    let (op, _) := op.reprPrec
+    let x := cont ++ nest (Exp.printTree x)
+    let y := last ++ nest (Exp.printTree y)
+    join [s!"({op})", x, y]
+  | .unaryOp op x =>
+    let (op, _) := op.reprPrec
+    let x := last ++ nest (Exp.printTree x)
+    join [s!"({op})", x]
+  | .ifExp cond thn els =>
+    let cond := cont ++ nest (Exp.printTree cond)
+    let thn := cont ++ nest (Exp.printTree thn)
+    let els := last ++ nest (Exp.printTree els)
+    join ["(ite)", cond, thn, els]
+  | _ => e.reprPrec 0
+where
+  nest x := Std.Format.nest 5 x
+  join (l : List Std.Format) := Std.Format.joinSep l "\n"
+  cont := " |---"
+  last := " '---"
+
+end
+
+def Stmt.printExpTree (s : Stmt) : Std.Format :=
+  match s.stmt with
+  | .exp e => Exp.printTree e
+  | _ => "not an expression"
+
+/--
+Print expression trees, used to debug associativity
+-/
+def Prog.printExpTree (p : Prog) : Std.Format :=
+  let es := p.stmts.map Stmt.printExpTree
+  Std.Format.joinSep es "\n"
+
+def input := "1 + 1 * - (not 3)"
+-- def input := "(1 + 2) * 3"
+-- #eval run input "<input>"
+#eval (run input "<input>").map Stmt.printExpTree
+
+end Debug
