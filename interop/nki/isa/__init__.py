73420ef6e8f0a42778a004c401f38b13335b93d1
Pavel Potapov <ppotapov@amazon.com>
2025-09-17 06:33:26 -0400

    Feat: support both neuronxcc and nki namespaces

---
+++ interop/nki/isa/__init__.py
@@ -0,0 +1,15 @@
+# KLR implemetations of NKI ISA APIs
+
+def psum_raw_ptr(address, size):
+  p_start, f_start = address
+  p_size, f_size = size
+  p_end = p_start + p_size
+  f_end = f_start + f_size
+  return psum[p_start:p_end, f_start:f_end]
+
+def sbuf_raw_ptr(address, size):
+  p_start, f_start = address
+  p_size, f_size = size
+  p_end = p_start + p_size
+  f_end = f_start + f_size
+  return sbuf[p_start:p_end, f_start:f_end]
