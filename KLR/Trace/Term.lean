8dcd5a32382dc5641cca158070ecb808722ed012
Paul Govereau <govereau@amazon.com>
2025-11-18 12:11:48 -0500

    fix: format error message strings

--- KLR/Trace/Term.lean
+++ KLR/Trace/Term.lean
@@ -284,10 +284,10 @@ def listAccess (l : List Term) : List Term -> Err Term
       else throw "index out of bounds."
   | [.int i] => do
       let i := if i < 0 then l.length + i else i
-      if i < 0 then throw "negative index ({i}) out of bounds"
+      if i < 0 then throw s!"negative index ({i}) out of bounds"
       let n := i.toNat
       if h:l.length > n then return l.get (Fin.mk n h)
-      else throw "index ({n}) out of bounds"
+      else throw s!"index ({n}) out of bounds"
   | [.slice start u step] => do
       let start := start.getD 0
       let e := u.getD l.length
